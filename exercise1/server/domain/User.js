d3f10bb6d6c896fe2b8018db32ad282306b7f9d7
Jessica Marcus <jessica.marcus@gmail.com>
2014-10-27 16:41:04 -0400

    quest 5

--- exercise1/server/domain/User.js
+++ exercise1/server/domain/User.js
@@ -6,6 +6,8 @@ var User = function(params) {
     this.id = nextId++;
     this.firstName = params.firstName;
     this.lastName = params.lastName;
+    this.age = params.age;
+    this.emailAddress = params.emailAddress;
     this.username = params.username;
     this.password = params.password;
     this.role = (typeof params.role === 'undefined') ? 2 : params.role; // ['admin', 'author', 'subscriber']
--- exercise1/server/fixtures.js
+++ exercise1/server/fixtures.js
@@ -9,18 +9,24 @@ var fixtures = (function() {
         var users = [{
             firstName: 'Dan',
             lastName: 'Johnson',
+            age: '23',
+            emailAddress: 'dan@j.com',
             username: 'user1',
             password: 'test123',
             role: 0
         }, {
             firstName: 'Ella',
             lastName: 'Johnson',
+            age: '5',
+            emailAddress: 'ella@j.com',
             username: 'user2',
             password: 'test123',
             role: 1
         }, {
             firstName: 'Ava',
             lastName: 'Johnson',
+            age: '17',
+            emailAddress: 'ava@j.com',
             username: 'user3',
             password: 'test123'
         }];
